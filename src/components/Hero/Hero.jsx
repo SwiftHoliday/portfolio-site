d024ef783314628e9ea379172bad643d4a9f2717
Jonathyn Carter <jonathyncarter@gmail.com>
2021-07-06 15:10:51 -0400

    adds new animation to Hero text

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -2,6 +2,8 @@ import React, { useContext, useState, useEffect } from 'react';
 import { Container } from 'react-bootstrap';
 import Fade from 'react-reveal/Fade';
 import Roll from 'react-reveal/Roll';
+import Zoom from 'react-reveal/Zoom';
+import RubberBand from 'react-reveal/RubberBand';
 import LightSpeed from 'react-reveal/LightSpeed';
 import { Link } from 'react-scroll';
 import PortfolioContext from '../../context/context';
@@ -26,14 +28,16 @@ const Header = () => {
   return (
     <section id="hero" className="jumbotron">
       <Container>
-        <Roll left={isDesktop} bottom={isMobile} duration={1000} delay={500} distance="30px">
-          <h1 className="hero-title">
-            {title || 'Hi, my name is'}{' '}
-            <span className="text-danger">{name || 'Jonathyn Carter'}</span>
-            <br />
-            {subtitle || "I build things that make life better!"}
-          </h1>
-        </Roll>
+        <Zoom bottom>
+          <RubberBand left={isDesktop} bottom={isMobile} duration={1000} delay={500} distance="30px">
+            <h1 className="hero-title">
+              {title || 'Hi, my name is'}{' '}
+              <span className="text-danger">{name || 'Jonathyn Carter'}</span>
+              <br />
+              {subtitle || "I build things that make life better!"}
+            </h1>
+            </RubberBand>
+          </Zoom>
         <LightSpeed right={isDesktop} bottom={isMobile} duration={1000} delay={1610} distance="30px">
           <p className="hero-cta">
             <span className="cta-btn cta-btn--hero">
